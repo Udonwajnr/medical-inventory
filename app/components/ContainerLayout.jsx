fb257ba7501cbdbfb080ae6aa4fa29ed251dddd7
Umoh Udonwa <udonwajnr10@gmail.com>
2024-08-30 02:18:09 -0700

    commit

---
+++ app/components/ContainerLayout.jsx
@@ -0,0 +1,7 @@
+export default function ContainerLayout(){
+    return(
+        <>
+            
+        </>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -9,28 +9,7 @@ import {
 export default function Home() {
   return (
     <main className="">
-      <Accordion type="single" collapsible className="w-full">
-      <AccordionItem value="item-1">
-        <AccordionTrigger>Is it accessible?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It adheres to the WAI-ARIA design pattern.
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value="item-2">
-        <AccordionTrigger>Is it styled?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It comes with default styles that matches the other
-          components&apos; aesthetic.
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value="item-3">
-        <AccordionTrigger>Is it animated?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It&apos;s animated by default, but you can disable it if you
-          prefer.
-        </AccordionContent>
-      </AccordionItem>
-    </Accordion> 
+      <h1>Hello and Welcome to Tuky inventory system</h1>
     </main>
   );
 }
